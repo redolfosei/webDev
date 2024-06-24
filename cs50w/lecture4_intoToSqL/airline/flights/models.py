a89901b565678724b1829d6e90dad56fb6368482
RedolF <redolf639@gmail.com>
2024-06-24 23:11:57 +0000

    flights added to passengers and displayed on flight.html successfully

--- cs50w/lecture4_intoToSqL/airline/flights/models.py
+++ cs50w/lecture4_intoToSqL/airline/flights/models.py
@@ -20,8 +20,10 @@ class Flight(models.Model):
     return f"{self.id}, {self.origin}, {self.destination}, {self.duration}"
   
   #passenger class 
-  
 class Passenger(models.Model):
   first = models.CharField(max_length=64)
   last = models.CharField(max_length=64)
+  flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
   
+  def __str__(self):
+    return f"{self.first} {self.last}"
